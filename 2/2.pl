ef20befc7b9eca37eb02787875a68e27503d931f
tomas <tomas.kozakas@gmail.com>
2023-09-18 16:37:35 +0300

    Changes

--- 2/2.pl
+++ 2/2.pl
@@ -16,20 +16,22 @@ studentas(rolandas, 4).
 % yraVyresnis(StudentoVardas1, StudentoVardas2). Sąryšis „būti vyresniu“ yra tranzityvus,
 % todėl į faktų aibę neįtraukiami tie faktai, kurie seka iš tranzityvumo sąryšio.
 % Tai reiškia, kad, jeigu Jonas vyresnis už Miglę, o Miglė % už Ritą, tai faktų aibėje yra
-% predikatai yraVyresnis(jonas, migle) ir yraVyresnis(migle, rita), bet nėra predikato yraVyresnis(jonas,
-% rita). Taip pat sąryšis „būti vyresniu“ yra pilnai sutvarkantis studentų aibę. Tai reiškia,
-% kad, kokius bepaimsime du skirtingus studentus Studentas1 ir Studentas2, arba Studentas1 bus
-% vyresnis už Studentas2, arba Studentas2 bus vyresnis už Studentas1. Taigi, pavyzdžiui, tokia
-% faktų aibė yra negalima: yraVyresnis(jonas, migle), yraVyresnis(migle, rita),
-% yraVyresnis(jonas, tadas) ir yraVyresnis(tadas, rita). Apibrėžkite predikatą:
+% predikatai yraVyresnis(jonas, migle) ir yraVyresnis(migle, rita), bet nėra predikato 
+% yraVyresnis(jonas, rita). Taip pat sąryšis „būti vyresniu“ yra pilnai sutvarkantis 
+% studentų aibę. Tai 
+% reiškia, kad, kokius bepaimsime du skirtingus studentus Studentas1 ir Studentas2, arba 
+% Studentas1 bus vyresnis už Studentas2, arba Studentas2 bus vyresnis už Studentas1. Taigi, 
+% pavyzdžiui, tokia faktų aibė yra negalima: yraVyresnis(jonas, migle), yraVyresnis(migle, 
+% rita), yraVyresnis(jonas, tadas) ir yraVyresnis(tadas, rita). Apibrėžkite predikatą:
 
 % studentas A yra vyresnis už to paties kurso studentą B
 
-yraVyresis(studentoVardas1, studentoVardas2) :-
-    studentas(studentoVardas1, X),
-    studentas(studentoVardas2, Y),
-    studentoVardas1 /= studentoVardas2,
-    X = Y.
+yraVyresis(StudentoVardas1, StudentoVardas2) :-
+    studentas(StudentoVardas1, Kursas1, Amzius1),
+    studentas(StudentoVardas2, Kursas2, Amzius2),
+    StudentoVardas1 \= StudentoVardas2,
+    Kursas1 = Kursas2,
+    Amzius1 > Amzius2.
 
 
 
