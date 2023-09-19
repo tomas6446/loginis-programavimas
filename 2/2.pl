5d1dcf8337d43c80d8cdded4b69e3ce897f6bba9
tomas <tomas.kozakas@gmail.com>
2023-09-19 14:37:12 +0300

    2nd task commit

--- 2/2.pl
+++ 2/2.pl
@@ -1,16 +1,6 @@
 % Tomas Kozakas
 % 2 užduotis: 2.1; 6.4
 
-studentas(tomas, 3).
-studentas(algis, 3).
-studentas(benas, 3).
-studentas(nikita, 2).
-studentas(petras, 2).
-studentas(jonas, 2).
-studentas(gabija, 1).
-studentas(migle, 1).
-studentas(rolandas, 4).
-
 % (2.1)
 % Duomenų bazėje saugomi duomenys apie studentus faktais: studentas(Vardas, Kursas);
 % yraVyresnis(StudentoVardas1, StudentoVardas2). Sąryšis „būti vyresniu“ yra tranzityvus,
@@ -24,14 +14,50 @@ studentas(rolandas, 4).
 % pavyzdžiui, tokia faktų aibė yra negalima: yraVyresnis(jonas, migle), yraVyresnis(migle, 
 % rita), yraVyresnis(jonas, tadas) ir yraVyresnis(tadas, rita). Apibrėžkite predikatą:
 
+% studentas(Vardas, Kursas)
+studentas(tomas, 3).
+studentas(algis, 3).
+studentas(benas, 3).
+studentas(nikita, 2).
+studentas(petras, 2).
+studentas(jonas, 2).
+studentas(gabija, 1).
+studentas(migle, 1).
+studentas(rolandas, 4).
+
 % studentas A yra vyresnis už to paties kurso studentą B
+vyresnis(tomas, algis).
+vyresnis(algis, benas).
 
-yraVyresis(StudentoVardas1, StudentoVardas2) :-
-    studentas(StudentoVardas1, Kursas1, Amzius1),
-    studentas(StudentoVardas2, Kursas2, Amzius2),
-    StudentoVardas1 \= StudentoVardas2,
-    Kursas1 = Kursas2,
-    Amzius1 > Amzius2.
+yraVyresnis(StudentoVardas1, StudentoVardas2) :-
+    	vyresnis(StudentoVardas1, StudentoVardas2).
+yraVyresnis(StudentoVardas1, StudentoVardas2) :-
+    	vyresnis(StudentoVardas1, X),
+    	yraVyresnis(X, StudentoVardas2).
+    
+    
+% Natūralieji skaičiai yra modeliuojami termais nul, s(nul), s(s(nul)),… (žr. paskaitos medžiagą). 
+% Apibrėžkite predikatą: pirmasis skaičius dalus antrajam.
+subtract(X, nul, X).		% atimties bazinė sąlyga 
 
+% Rekursinė atimties sąlyga
+% atimame po vieneta
+subtract(s(X), s(Y), Z) :-
+    subtract(X, Y, Z).
+    
+subtract(X, nul, X).        % atimties bazinė sąlyga 
 
+% Rekursinė atimties sąlyga
+% atimame po vieneta
+subtract(s(X), s(Y), Z) :-
+    subtract(X, Y, Z).
+
+dalus(X, X).
+dalus(X, Y) :-
+    subtract(X, Y, Z), 
+    X \= Z,           
+    dalus(Z, Y). 
+    
+% 6 / 2 dalus(s(s(s(s(s(s(nul)))))),s(s(nul))) -> true
+% 5 / 2 dalus(s(s(s(s(s(nul))))),s(s(nul))) -> false
 
